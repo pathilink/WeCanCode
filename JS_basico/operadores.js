da0386620fa9035f23f27766b51919d2cf1efde1
pathilink <pathilink@gmail.com>
2021-01-02 20:04:09 -0300

    add JS_basico folder

---
+++ JS_basico/operadores.js
@@ -0,0 +1,19 @@
+var operador = 100;
+var operando = 50;
+var resultado = operador * operando;
+console.log(resultado);
+
+var nome = "Patrícia";
+var sobrenome = "do Nascimento";
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+var primeiroValor = 10;
+var segundoValor = "azul";
+var resultado = primeiroValor - segundoValor;
+console.log(resultado);
+
+var primeiroValor = 10;
+var segundoValor = "3";
+var resultado = primeiroValor - segundoValor;
+console.log(resultado);
